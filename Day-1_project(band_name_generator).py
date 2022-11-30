f6730306df12c64c2fc8bdd7778b54cd10ddf519
GodfredAwusi <4231220071@live.gctu.edu.gh>
2022-11-30 00:06:28 +0000

    Initial commit

---
+++ Day-1_project(band_name_generator).py
@@ -0,0 +1,15 @@
+# Create a band name generator. The program should ask the city of the user and the the user's pet name. Then combine the two names to make a band name
+
+# print("Hello,\nWelcome to Cosby Band Name Generator")
+# print("What city did you grow up in?")
+# city_name = input()
+
+# print("What is the name of your favorite pet")
+# pet_name = input()
+
+# print("Your band name could be " + city_name + ' ' + pet_name)
+
+print("Hello,\nWelcome to Cosby Band Name Generator")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your favorite pet?\n")
+print("Your band name could be " + city_name + " " + pet_name)
---
+++ README.md
@@ -0,0 +1,2 @@
+# Band Name Generator
+Create a band name generator. The program should ask the city of the user and the the user's pet name. Then combine the two names to make a band name
